0719209a66694dba3e05fea6ae681916edfe66c7
saraheulie <79435002+saraheulie@users.noreply.github.com>
2021-03-16 18:05:32 -0500

    form 1

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,12 @@
-/*
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+btnSubmit.onclick=function(){
+  let name = inptUsername.value
+  let n = members.includes(name)
+  if (n === True) {
+    lblMessage.value = `${name}, you are a member!`
+  } else {
+    members.push(name)
+    lblMessage.value = `${name}, you are currently not a member, but you have been added to the list!`
+  }
+}
