83fbb4b8d6dcb20236669ba85f08863b9c2f6e95
saraheulie <79435002+saraheulie@users.noreply.github.com>
2021-03-16 22:32:01 -0500

    updates

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,14 +1,22 @@
-
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
 
-btnSubmit.onclick=function(){
-  let user_name = inptUsername.value
-  if (members.includes(inptUsername.value)) {
-    lblMessage.value = `${inptUsername.value}, you are a member!`
-  } else {
-    lblMessage.value = `${inptUsername.value}, you are currently not a member, but you have been added to the list!`
-    members.push(inptUsername.value)
-  }
+btnAdd.onclick=function(){
+  let name = inptUsername.value
+if (members.includes(name)) {
+  lblMessage.value = `${name}, you are a member!`
+} else { 
+  lblMessage.value = `${name}, you are not a member, you have been added to the list!`
+  members.push(name)
 }
-
-
+}
+/*
+btnAdd.onclick=function(){
+  let addition = inptNewName.value
+if (members.includes(inptNewName.value)) {
+  lblOutput.value = "You are a member"
+} else { 
+  lblOutput.value = "You are not a member, adding"
+  members.push(inptNewName.value)
+}
+}
+*/
